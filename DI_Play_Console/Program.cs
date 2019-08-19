1946e04927ea14fb908dab0b0fbb777c287b1b6d
Robert Jernberg <robert.jernberg@gmail.com>
2019-08-19 18:57:53 +0200

    Add console code to test internal configurable library service
    Also cleaned up the code and improved usage.

--- DI_Play_Console/Program.cs
+++ DI_Play_Console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using DI_Play_Lib.Configuration;
 using DI_Play_Lib.Extensions;
 using DI_Play_Lib.Services;
@@ -14,46 +15,118 @@ namespace DI_Play_Console
             // Base code taken from this blog post: https://andrewlock.net/using-dependency-injection-in-a-net-core-console-application/
 
             // setup DI.
-            var serviceProvider = new ServiceCollection()
+            var serviceCollection = new ServiceCollection()
                 .AddTransient<ITransientService, TransientService>()
                 .AddSingleton(new ServiceConfiguration())
                 .AddScoped<IScopedService, ScopedService>()
                 .AddSingleton<ISingletonService, SingletonService>()
-                .AddDIPlayService()
-                .BuildServiceProvider();
+                .AddDIPlayService();
 
-            Console.WriteLine("Test of added library service:");
-            var diPlayService = (ISimpleLibService)serviceProvider.GetService(typeof(ISimpleLibService));
-            Console.WriteLine(diPlayService.GetMessage() + Environment.NewLine);
+            var serviceProvider = serviceCollection.BuildServiceProvider();
 
             while (true)
             {
-                UserServices(serviceProvider);
+                Console.Clear();
+                Console.WriteLine("Select test to run:");
+                Console.WriteLine("1: Test Service Scopes");
+                Console.WriteLine("2: Test Library Service");
+                Console.WriteLine("3/4: Test Library Service with {lib/app} config");
 
-                Console.WriteLine("Press q to exit. Press any other key to run again.");
-                var key = Console.ReadKey();
-                if (key.KeyChar == 'q') break;
+                var testKey = Console.ReadKey().KeyChar;
+                Console.WriteLine("");
+                switch (testKey)
+                {
+                    case '1':
+                        TestServiceScopes(serviceProvider);
+                        break;
+                    case '2':
+                        TestLibraryService(serviceProvider);
+                        break;
+                    case '3':
+                        TestOverridingLibraryService(serviceCollection, false);
+                        break;
+                    case '4':
+                        TestOverridingLibraryService(serviceCollection, true);
+                        break;
+                    default:
+                        Console.WriteLine("Incorrect option selected!");
+                        break;
+                }
+
+                Console.WriteLine("Press q to exit program. Press any other key to select a new test.");
+                if (Console.ReadKey().KeyChar == 'q') break;
             }
 
         }
 
+        private static void TestLibraryService(ServiceProvider serviceProvider)
+        {
+            CallServiceDetailed<ISimpleLibService>(serviceProvider);
+        }
+
+        private static void TestOverridingLibraryService(IServiceCollection services, bool useAppConfig)
+        {
+            RemoveService<IInternalServiceConfiguration>(services);
+
+            // This is only supposed to be done during setup. We do it here at run time merely as an example of how to do it.
+            if (useAppConfig)
+                services.AddSingleton<IInternalServiceConfiguration, AppConfig>();
+            else
+                services.AddSingleton<IInternalServiceConfiguration, InternalServiceConfiguration>();
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            CallServiceDetailed<IConfigurableLibService>(serviceProvider);
+        }
+
+        public class AppConfig : IInternalServiceConfiguration
+        {
+            public string MessagePrefix => "App config!";
+        }
+
+        private static void CallServiceDetailed<T>(ServiceProvider serviceProvider) where T : IBaseService
+        {
+            Console.WriteLine($"Test call to service of type {typeof(T).Name}.");
+            var service = (T)serviceProvider.GetService(typeof(T));
+            Console.WriteLine($"Got implementation {service.GetType().Name}.");
+            Console.WriteLine(service.GetMessage() + Environment.NewLine);
+        }
+
+        private static void RemoveService<T>(IServiceCollection services)
+        {
+            var libconfig = services.FirstOrDefault(descriptor => descriptor.ServiceType == typeof(T));
+            services.Remove(libconfig);
+        }
+
+        private static void TestServiceScopes(ServiceProvider serviceProvider)
+        {
+            while (true)
+            {
+                UserServices(serviceProvider);
+
+                Console.WriteLine("Press any other key to run again and see which services gets a new instance. Press q to exit.");
+                var key = Console.ReadKey();
+                Console.WriteLine("");
+                if (key.KeyChar == 'q') break;
+            }
+        }
+
         private static void UserServices(ServiceProvider serviceProvider)
         {
-            // Retrive services.
-            var transientService = (ITransientService)serviceProvider.GetService(typeof(ITransientService));
-            var transient2 = (ITransientService)serviceProvider.GetService(typeof(ITransientService));
-            var scopedService = (IScopedService)serviceProvider.GetService(typeof(IScopedService));
-            var scoped2 = (IScopedService)serviceProvider.GetService(typeof(IScopedService));
-            var singletonService = (ISingletonService)serviceProvider.GetService(typeof(ISingletonService));
-            var singleton2 = (ISingletonService)serviceProvider.GetService(typeof(ISingletonService));
+            CallServiceSimple<ITransientService>(serviceProvider);
+            CallServiceSimple<ITransientService>(serviceProvider);
 
-            // Call retrived services.
-            Console.WriteLine(transientService.GetMessage());
-            Console.WriteLine(transient2.GetMessage());
-            Console.WriteLine(scopedService.GetMessage());
-            Console.WriteLine(scoped2.GetMessage());
-            Console.WriteLine(singletonService.GetMessage());
-            Console.WriteLine(singleton2.GetMessage());
+            CallServiceSimple<IScopedService>(serviceProvider);
+            CallServiceSimple<IScopedService>(serviceProvider);
+
+            CallServiceSimple<ISingletonService>(serviceProvider);
+            CallServiceSimple<ISingletonService>(serviceProvider);
+        }
+
+        private static void CallServiceSimple<T>(ServiceProvider serviceProvider) where T : IBaseService
+        {
+            var service = (T)serviceProvider.GetService(typeof(T));
+            Console.WriteLine(service.GetMessage());
         }
     }
 }
